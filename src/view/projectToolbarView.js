32a3f63f1331f525a546a12f3b0666bf63839b0d
Juuso Määttä <juusomaatta@hotmail.fi>
2024-11-20 20:16:00 +0200

    Create project settings modal style

--- src/view/projectToolbarView.js
+++ src/view/projectToolbarView.js
@@ -1,9 +1,21 @@
 export function handleProjectToolbarView(user){
     user.pubSub.subscribe('project-change', handleProjectChange);
 
-    const projectTitle = document.querySelector(".project-toolbar-title");
+    const projectToolbar = document.querySelector(".project-toolbar-container");
+    const projectTitle = projectToolbar.querySelector(".project-toolbar-title");
+    const projectSettings = projectToolbar.querySelector(".project-toolbar-settings");
+    projectSettings.addEventListener("click", handleProjectSettings);
+
+    const projectSettingsModal = document.querySelector(".project-settings");
 
     function handleProjectChange(project){
         projectTitle.textContent = project.title;
+    }
+
+    function handleProjectSettings(){
+        projectSettingsModal.showModal();
+        const rect = projectToolbar.getBoundingClientRect();
+        projectSettingsModal.style.top = rect.bottom +"px";
+        projectSettingsModal.style.left = rect.left +"px";
     }
 }
